cf871d4c2a53b3351981c6f469a96f92b4e94144
shivam shankhdhar <39639414+shivamShankhdhar@users.noreply.github.com>
2023-07-08 10:28:26 +0530

    sql left outer join

--- joins.sql
+++ joins.sql
@@ -19,4 +19,13 @@ e.employee_id,e.last_name,e.salary,d.department_id,d.department_name,l.city
 from employees e INNER JOIN departments d
 on (e.department_id = d.department_id)
 INNER JOIN locations l
-on (d.location_id = l.location_id ) and l.city='Toronto';
+on (d.location_id = l.location_id ) and e.last_name like '%H%';
+
+select
+e.employee_id,e.last_name,e.salary,d.department_id,d.department_name
+from employees e LEFT OUTER JOIN departments d
+on (e.department_id = d.department_id);
+-- INNER JOIN locations l
+-- on (d.location_id = l.location_id ) and e.last_name like '%H%';
+
+
