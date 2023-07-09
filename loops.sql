b9ee642af455261c5299320723723e2fa8ddc10f
shivam shankhdhar <39639414+shivamShankhdhar@users.noreply.github.com>
2023-07-09 16:28:17 +0530

    loop leave iterate

--- loops.sql
+++ loops.sql
@@ -13,8 +13,9 @@ end $$
 
 call print_val(1);
 -- loop,leave,iterate
+-- Q1:pattern 1,2,3,4,5
 delimiter $$ 
-create procedure print_val2(IN x int,INOUT str varchar(10))
+create procedure print_val3(IN x int,INOUT str varchar(16383))
 begin
 print1:loop
 if x >5 then
@@ -27,7 +28,27 @@ iterate print1;
 end if;
 end loop;
 end $$
-
+drop procedure print_val3;
 set @str = '';
-call print_val2(1,@str);
+call print_val3(1,@str);
+select @str;
+
+-- Q2: pattern 1,5,25,625,3125
+delimiter $$ 
+create procedure print_val3(IN x int,INOUT str varchar(16383))
+begin
+print1:loop
+if x >3125 then
+leave print1;
+end if;
+if x <=3125 then 
+	set str = concat(str,x,',');
+    set x = x * 5;
+iterate print1;
+end if;
+end loop;
+end $$
+drop procedure print_val3;
+set @str = '';
+call print_val3(1,@str);
 select @str;
